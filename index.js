58a4c19173871b89a94b5e5ed23ecbdeba6acde5
mukul-bhalla <bhallamukul27@gmail.com>
2023-10-03 20:14:39 +0530

    Added Home and User Page

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require('path')
 
 const User = require('./models/user')
 const Transactions = require('./models/transactions')
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const dbUrl = process.env.DB_URL
 mongoose.connect(dbUrl)
@@ -16,13 +17,22 @@ mongoose.connect(dbUrl)
     .catch((error) => {
         console.error("MongoDB Connection Error:", error);
     });
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hii!');
+
+app.get('/', async (req, res) => {
+    const allUser = await User.find({});
+    res.render("home", { allUser });
+})
+
+app.get('/user/:id', async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.render("user", { user })
 })
 
 app.listen(3000, () => {
