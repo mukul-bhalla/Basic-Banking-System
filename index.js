9e97cd5ee7f81fff7a9b82345b3c8eda75d2fd3e
mukul-bhalla <bhallamukul27@gmail.com>
2023-10-04 23:14:31 +0530

    Added Transfer functionality

--- index.js
+++ index.js
@@ -35,6 +35,44 @@ app.get('/user/:id', async (req, res) => {
     res.render("user", { user })
 })
 
+app.get('/success', (req,res)=>{
+    res.render("success");  
+})
+
+app.post('/transaction/user/:id',async (req,res)=>{
+    // console.log(req.body)
+    const {id}= req.params;
+    const {receiver} = req.body;
+    const amount = parseInt(req.body.amount)
+    const send = await User.findById(id);
+    const receive = await User.findOne({name:receiver});
+    if(send.current_balance<amount){
+        res.redirect("/error")
+    }
+    const sendBalance = send.current_balance;
+    const receiveBalance = receive.current_balance;
+    const receiverId = receive._id; 
+    // console.log(send)
+    // console.log(receive)
+    // console.log(amount)
+    // console.log(sendBalance)
+    // console.log(receiveBalance)
+
+    // console.log(receiverId);
+    await User.findByIdAndUpdate(id,{current_balance:sendBalance-amount});
+    await User.findByIdAndUpdate(receiverId,{current_balance:receiveBalance+amount});
+    
+    res.redirect("/success");
+})
+
+app.get('/transaction/user/:id', async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    const allUser = await User.find({});
+    res.render("transaction", { user, allUser });
+
+})
+
 app.listen(3000, () => {
     console.log("Listening at port 3000")
 })
