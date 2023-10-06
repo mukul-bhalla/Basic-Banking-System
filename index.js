0e6007bf8d9167bbf4a1f120ce5ba629897e84fb
mukul-bhalla <bhallamukul27@gmail.com>
2023-10-06 17:35:03 +0530

    Added History Functionality(Incomplete)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express');
 const app = express();
 const path = require('path')
-
+const bodyParser = require('body-parser');
 const User = require('./models/user')
 const Transactions = require('./models/transactions')
 const ejsMate = require('ejs-mate');
@@ -35,33 +35,53 @@ app.get('/user/:id', async (req, res) => {
     res.render("user", { user })
 })
 
-app.get('/success', (req,res)=>{
-    res.render("success");  
+app.get('/success', (req, res) => {
+    res.render("success");
 })
 
-app.post('/transaction/user/:id',async (req,res)=>{
+app.get('/history/user/:id', async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id).populate("transaction")
+    console.log(user)
+    res.render("history", { user });
+})
+
+app.post('/transaction/user/:id', async (req, res) => {
     // console.log(req.body)
-    const {id}= req.params;
-    const {receiver} = req.body;
+    const { id } = req.params;
+    const { receiver } = req.body;
     const amount = parseInt(req.body.amount)
     const send = await User.findById(id);
-    const receive = await User.findOne({name:receiver});
-    if(send.current_balance<amount){
+    // console.log(req.body)
+    if (send.current_balance < amount) {
+        console.log("Inside If")
         res.redirect("/error")
     }
+    // console.log("Outside if")
+    const receive = await User.findOne({ name: receiver });
     const sendBalance = send.current_balance;
     const receiveBalance = receive.current_balance;
-    const receiverId = receive._id; 
-    // console.log(send)
-    // console.log(receive)
-    // console.log(amount)
-    // console.log(sendBalance)
-    // console.log(receiveBalance)
+    const receiverId = receive._id;
 
-    // console.log(receiverId);
-    await User.findByIdAndUpdate(id,{current_balance:sendBalance-amount});
-    await User.findByIdAndUpdate(receiverId,{current_balance:receiveBalance+amount});
-    
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    // This arrangement can be altered based on how we want the date's format to appear.
+    let currentDate = `${day}-${month}-${year}`;
+    const transact = new Transactions({
+        payer: send.name,
+        receiver: receive.name,
+        amount: amount,
+        date: currentDate
+    })
+    await transact.save();
+    send.transaction.push(transact)
+    await User.findByIdAndUpdate(id, { current_balance: sendBalance - amount });
+    await User.findByIdAndUpdate(receiverId, { current_balance: receiveBalance + amount });
+
+
     res.redirect("/success");
 })
 
